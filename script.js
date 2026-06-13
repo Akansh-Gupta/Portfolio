03acdaf25ddc137d1911ab44d20b0f95a102c890
Akansh-Gupta <akg.gupta365@gmail.com>
2026-06-14 01:51:53 +0530

    Updated PFP

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ viewBox="0 -960 960 960" width="30px" fill="#e8eaed">
 </svg></li>
 </ul>`
 }
-
 function Sidebar() {
     sidebar.innerHTML = `<li onclick="hideSidebar()"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
 <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
